ff7d06fd54a1422039355b7224ddfb2acb236063
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-05-02 08:42:04 +0600

    fixed the contact type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import TablePagination from "@/components/TablePagination";
 import { ContactDataType } from "@/lib/types";
-import { successToast, errorToast } from "@/lib/utils";
+import { errorToast, successToast } from "@/lib/utils";
 import { AuthContext } from "@/providers/AuthProvider";
 import Image from "next/image";
 import { useContext, useEffect, useRef, useState } from "react";
@@ -32,7 +32,6 @@ const Dashboard = () => {
         setPages(result.pagination.pages);
         setPage(result.pagination.page);
       }
-      console.log(result);
     };
 
     user?.role === "Admin" && loadContacts();
--- src/models/contacts.ts
+++ src/models/contacts.ts
@@ -1,7 +1,7 @@
-import { ContactType } from "@/lib/types";
+import { ContactDataType } from "@/lib/types";
 import mongoose, { models } from "mongoose";
 
-const contactSchema = new mongoose.Schema<ContactType>({
+const contactSchema = new mongoose.Schema<ContactDataType>({
   name: { type: String, required: [true, "Name is required"] },
   email: { type: String, required: [true, "Eiail is required"] },
   phone: { type: String, required: [true, "Phone is required"] },
